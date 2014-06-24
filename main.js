4eda10142091fe2c18c530bb441d44c3e0173dad
dlsso <supertonesfan@hotmail.com>
2014-06-24 11:12:34 -0600

    Solved 3 and 4

--- main.js
+++ main.js
@@ -1,16 +1,33 @@
 // Problem 1
 var tripleFive = function() {
 	for (i = 0; i <=2; i++){
-		console.log("Five!")
+		console.log("Five!");
 	}
 }
 
+console.log("1.")
 tripleFive()
+
 
 // Problem 2
 var lastLetter = function(string) {
 	return string.charAt(string.length -1);
 }
 
-console.log("Run lastLetter('some string') to test")
+console.log("2. Run lastLetter('some string') to test")
 
+
+// Problem 3
+var square = function(x) {
+	return x*x;
+}
+
+console.log("3. Run square(x) to test")
+
+
+// Problem 4
+var negate = function(x) {
+	return -x;
+}
+
+console.log("4. Run negate(x) to test")
